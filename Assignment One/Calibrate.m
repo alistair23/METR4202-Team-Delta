60d4565f3bc35942e698d7b81ba6805e6fe44596
Alistair Francis <alistair@alistiar23.me>
2013-08-27 10:27:50 +1000

    Calibration and Forward Kinematics

---
+++ Assignment One/Calibrate.m
@@ -0,0 +1,64 @@
+% METR4202 Team Project One
+% Team: Delta (Group One)
+% The University of Queensland
+
+function Calibrate()
+
+%% verify that the RWTH - Mindstorms NXT toolbox is installed.
+if verLessThan('RWTHMindstormsNXT', '4.01');
+    error(strcat('This program requires the RWTH - Mindstorms NXT Toolbox ' ...
+    ,'version 4.01 or greater. Go to http://www.mindstorms.rwth-aachen.de ' ...
+    ,'and follow the installation instructions!'));
+end%if
+
+
+%% Prepare
+COM_CloseNXT all
+close all
+
+%% Connect to NXT via USB
+h = COM_OpenNXT('USB.ini');
+COM_SetDefaultNXT(h);
+
+%% Set params
+power = 80;
+port = [MOTOR_A; MOTOR_B; MOTOR_C];  % motorports to control the delta robot
+
+%% Create motor objects
+mUpOne    = NXTMotor(port(1), 'Power',  power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+mDownOne  = NXTMotor(port(1), 'Power', -power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+
+mUpTwo    = NXTMotor(port(2), 'Power',  power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+mDownTwo  = NXTMotor(port(2), 'Power', -power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+
+mUpThree    = NXTMotor(port(3), 'Power',  power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+mDownThree  = NXTMotor(port(3), 'Power', -power, 'ActionAtTachoLimit', 'Brake', 'SmoothStart', true);
+
+%% Prepare motor
+mUpOne.Stop('off');
+mUpTwo.Stop('off');
+mUpThree.Stop('off');
+mUpOne.ResetPosition();
+mUpTwo.ResetPosition();
+mUpThree.ResetPosition();
+
+%% BREAKPOINT!
+%% Manually move the motors to the origin
+
+%% Find current position
+data = mUpOne.ReadFromNXT();
+theta1  = data.Position;
+data = mUpTwo.ReadFromNXT();
+theta2  = data.Position;
+data = mUpThree.ReadFromNXT();
+theta2  = data.Position;
+
+%% BREAKPOINT! - Write down the values above
+%% Return the motors to zero
+MoveMotors(0, 0, 0);
+
+% Use the forward kinematics to calculate a x, y and z offset and enter
+% that into the main function
+
+end
+
