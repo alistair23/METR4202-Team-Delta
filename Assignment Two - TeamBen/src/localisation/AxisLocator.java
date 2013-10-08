07d93a5f24bf8ac80691945fd0976e505d166c42
BlackCircuits <ben@nestermix.com>
2013-10-08 12:45:17 +1000

    AxisLocator
    Nyartoolkit must be added to the build path.

--- Assignment Two - TeamBen/src/localisation/AxisLocator.java
+++ Assignment Two - TeamBen/src/localisation/AxisLocator.java
@@ -2,19 +2,11 @@ package localisation;
 
 import static com.googlecode.javacv.cpp.opencv_highgui.cvLoadImage;
 
-import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Enumeration;
-
-import javax.media.j3d.Background;
-import javax.media.j3d.ImageComponent;
-import javax.media.j3d.ImageComponent2D;
-import javax.media.j3d.Transform3D;
-import javax.media.j3d.TransformGroup;
-import javax.vecmath.Matrix4d;
 
 import com.googlecode.javacpp.Loader;
+import com.googlecode.javacv.cpp.opencv_core.CvMat;
 import com.googlecode.javacv.cpp.opencv_objdetect;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
@@ -24,8 +16,6 @@ import jp.nyatla.nyartoolkit.core.types.matrix.NyARDoubleMatrix44;
 import jp.nyatla.nyartoolkit.detector.NyARSingleDetectMarker;
 import jp.nyatla.nyartoolkit.java3d.utils.J3dNyARParam;
 import jp.nyatla.nyartoolkit.utils.j2se.NyARBufferedImageRaster;
-import jp.nyatla.nyartoolkit.core.*;
-import jp.nyatla.nyartoolkit.java3d.utils.*;
 
 
 public class AxisLocator {
@@ -35,9 +25,7 @@ public class AxisLocator {
 	  private NyARSingleDetectMarker detector;
 
 	  private NyARDoubleMatrix44 transMat = new NyARDoubleMatrix44();
-	  private TransformGroup tg;
-	  private Background bg;
-	  private ImageComponent2D imc2d;
+
 	  private final String CARCODE_FILE = "patt.kanji";
 
 	  private final double MARKER_SIZE = 0.035; // 35 mm width and height in Java 3D world units
@@ -66,8 +54,7 @@ public class AxisLocator {
 			      try {
 					raster = new NyARBufferedImageRaster(image.getBufferedImage());
 
-			      imc2d = new ImageComponent2D(ImageComponent2D.FORMAT_RGB, image.getBufferedImage(), true, false);
-			      imc2d.setCapability(ImageComponent.ALLOW_IMAGE_WRITE);
+
 			      
 			      NyARCode markerInfo = NyARCode.createFromARPattFile(new FileInputStream(CARCODE_FILE),16, 16);
 			      detector = NyARSingleDetectMarker.createInstance(cameraParams, markerInfo, MARKER_SIZE);
@@ -98,26 +85,37 @@ public class AxisLocator {
 		      try {
 				raster.wrapImage(image.getBufferedImage());
 				if (raster.hasBuffer()) {
-					if (bg != null) {
-						imc2d.set( raster.getBufferedImage());
-						bg.setImage(imc2d);
-					}
+
 					boolean foundMarker = detector.detectMarkerLite(raster, 120);
 					
 					if (foundMarker) {
 						detector.getTransmat(transMat);
 						System.out.println("Matrixing.");
-						Matrix4d matrix = new Matrix4d(
-								-transMat.m00, -transMat.m10, transMat.m20, 0,
-								-transMat.m01, -transMat.m11, transMat.m21, 0,
-								-transMat.m02, -transMat.m12, transMat.m22, 0,
-								-transMat.m03, -transMat.m13, transMat.m23, 1);
+						CvMat matrix = CvMat.create(4, 4);
+						
+						matrix.put(0, 0, -transMat.m00);
+						matrix.put(1, 0, -transMat.m10);
+						matrix.put(2, 0, transMat.m20);
+						matrix.put(3, 0, 0);
+
+						matrix.put(0, 1, -transMat.m01);
+						matrix.put(1, 1, -transMat.m11);
+						matrix.put(2, 1, transMat.m21);
+						matrix.put(3, 1, 0);
+
+						matrix.put(0, 2, -transMat.m02);
+						matrix.put(1, 2, -transMat.m12);
+						matrix.put(2, 2, transMat.m22);
+						matrix.put(2, 2, 0);
+
+						matrix.put(0, 3, -transMat.m03);
+						matrix.put(1, 3, -transMat.m13);
+						matrix.put(2, 3, transMat.m23);
+						matrix.put(3, 3, 1);
+
+		
 						System.out.println(matrix.toString());
-						matrix.transpose();
-						Transform3D t3d = new Transform3D(matrix);
-						if (tg != null){
-							tg.setTransform(t3d);
-						}
+
 					}else{
 						System.out.println("No Marker Found!");
 					}
