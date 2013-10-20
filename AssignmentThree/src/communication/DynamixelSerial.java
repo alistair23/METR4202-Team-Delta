1a96cf6238cb156efcf5a61b9001917ef028256b
BlackCircuits <ben@nestermix.com>
2013-10-20 11:41:22 +1000

    motors control done but not polished
    motors can be given an angle and they go to it. they can also have a
    varied speed. there is still a LOT of functionality left to implement
    int he motor class but you can move the motors to angles. so meh. not
    that the 'centre all motors' method is considered the zero angle point
    for kinematic calculations. this class would benefit from a "go here in
    this time" method. but i imagine that is taken care of by the
    kinematics.

--- AssignmentThree/src/communication/DynamixelSerial.java
+++ AssignmentThree/src/communication/DynamixelSerial.java
@@ -10,11 +10,29 @@ public class DynamixelSerial {
 	public static void main(String[] args) {
 		DynamixelSerial ds = new DynamixelSerial(4);
 		//System.out.println(ds.calcChecksum("0104022B01")); //FFFF0104022B01CC
-		ds.motor(3,1,150);
-		//ds.centermotors();
+		
+		
+		ds.motor(1,50,302);
+		//halt(1);
+		ds.motor(2,90,382);
+		//halt(1);
+		ds.motor(3,50,180);
+
+		halt(1100);
+		ds.motor(1,-50,302);
+		//halt(5);
+		ds.motor(2,-90,382);
+		//halt(5);
+		ds.motor(3,-50,180);
+		
+		halt(1100);
+		ds.centermotors();
 	}
 	
-	
+	public static void halt(int milli){
+		try {Thread.sleep(milli);} 
+		catch (InterruptedException e) {e.printStackTrace();}
+	}
 	
 	public DynamixelSerial (int port){
 		srl = new Serial(port);
@@ -54,11 +72,10 @@ public class DynamixelSerial {
 	}
 	
 	public void motor(int id, int pos, int speed){
-		int Apos = 0;
-		if(pos == 0){Apos = 512;}
-		else{
-			Apos = ((pos*512)/150)-150;
-		}
+
+		int Apos = (int) ((((float)pos)+150.0)*512.0/150.0);
+		if(Apos == 0 ){Apos = 512;}
+		System.out.println(Apos);
 		
 		if(id==0){write("FE","07","1E",xval(Apos)+xval(speed));} 
 		else{
