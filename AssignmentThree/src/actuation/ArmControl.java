7725d738b6e96efe71c4497e0208e1b29d1cb060
Alistair Francis <alistair@alistiar23.me>
2013-10-30 06:51:54 +1000

    Coin pick up Calibration
    Coin Calibration should now be working on the table

---
+++ AssignmentThree/src/actuation/ArmControl.java
@@ -0,0 +1,179 @@
+package actuation;
+
+import java.awt.Dimension;
+
+import communication.DynamixelSerial;
+
+public class ArmControl implements Runnable{
+	
+	DynamixelArm da = new DynamixelArm();
+	
+	
+	
+	private int Home = 140;
+	
+	int GLOBALOFFSET = -12;
+	
+	int tableHeight = 67;
+
+	int botHeight = 100;
+
+	int xoffset = 235;
+	
+	int speed = 150;
+	
+	int BoxS1 = 85;
+	int BoxS2 = -83;
+	
+	int Box1 = 130+GLOBALOFFSET;
+	int Box2 = 70+GLOBALOFFSET;
+	int Box3 = -120-GLOBALOFFSET;
+	int Box4 = -70-GLOBALOFFSET;
+	boolean atHome = false;
+
+	public ArmControl(){
+		//this.goHome();
+		da.port = 3;
+	}
+	public static void main(String[] args) {
+		ArmControl ac;
+		(new Thread(ac = new ArmControl())).start();
+		
+		ac.goHome();
+		//ac.get(160);
+		//ac.getCoin(50);
+		//ac.put(4);
+		//ac.get(170);
+		//ac.put(2);
+		//ac.get(170);
+		//ac.put(3);
+		//ac.get(170);
+		//ac.put(4);
+	}
+	
+	public void goHome(){
+		da.flip(false);
+		da.setXY(Home, botHeight+10, speed);
+		da.setXY(Home, botHeight+10, speed);
+		while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+		atHome = true;
+	}
+	
+	public void goTo(int x, int y){
+		if(x < 0){da.flip(true);}
+		else{da.flip(false);}
+		da.setXY(Math.abs(x), y, speed);
+		da.setXY(Math.abs(x), y, speed);
+		atHome = false;
+		while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+	}
+	
+	public void goTo(int x, int y, int ang){
+		if(x < 0){da.flip(true);}
+		else{da.flip(false);}
+		da.setXY(Math.abs(x), y, speed, ang);
+		da.setXY(Math.abs(x), y, speed, ang);
+		atHome = false;
+		while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+	}
+	
+	public void goTo(int x, int y, int ang, int speedy){
+		if(x < 0){da.flip(true);}
+		else{da.flip(false);}
+		da.setXY(Math.abs(x), y, speedy, ang);
+		da.setXY(Math.abs(x), y, speedy, ang);
+		atHome = false;
+		while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+	}
+	
+	public void get(int x){
+		if (x>215) {
+			x = 215;
+		}
+		if (x>200) {
+			goTo(x-20, botHeight+25, 0);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			goTo(x-15, tableHeight+20, 0, speed/3);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			goTo(x-10, tableHeight+15, 0, speed/3);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			//goTo(x-5, tableHeight+5, 0);
+			//while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			da.setXY(x, tableHeight, speed/2);
+			da.setXY(x, tableHeight, speed/2);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+		} else if (x > 185) {
+			goTo(x-10, tableHeight+15, 0);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			goTo(x-5, tableHeight+15, 0);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			da.setXY(x, tableHeight+5, speed/2);
+			da.setXY(x, tableHeight+5, speed/2);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+		} else if(x>180){
+			goTo(x-10, tableHeight+15, 0);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			goTo(x-5, tableHeight+15, 0);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			da.setXY(x, tableHeight, speed/2);
+			da.setXY(x, tableHeight, speed/2);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+		} else {
+			da.setXY(x, tableHeight+15, speed/2);
+			da.setXY(x, tableHeight+15, speed/2);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+			da.setXY(x, tableHeight+5, speed/2);
+			da.setXY(x, tableHeight+5, speed/2);
+			while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+		}
+
+		da.setXY(Home+30, botHeight+35, speed/3);
+		da.setXY(Home+30, botHeight+35, speed/3);
+		while(da.ds.readMoving()){DynamixelSerial.halt(10);}
+
+	}
+	
+	public void getCoin(int r){
+		int x = xoffset - r;
+		get(x);
+	}
+	
+	public void put(int num){
+		if(num == 1){
+			goTo(Box1, botHeight+10, 40);
+			goTo(Box1, botHeight-20, 0);
+			goTo(Box1+20, botHeight+20, 0);
+		}else if(num == 2){
+			goTo(Box2+60, botHeight+20, 0);
+			goTo(Box2+60, botHeight-15, 0);
+			goTo(Box2, botHeight-10, 0);
+			goTo(Box2, botHeight+40, 0);
+		}else if(num == 3){
+			goTo(Box3, botHeight+10, 40);
+			goTo(Box3, botHeight-23, 0);
+			goTo(Box3-20, botHeight+20, 0);
+		}else if(num == 4){
+			goTo(Box4-60, botHeight+20, 0);
+			goTo(Box4-60, botHeight-15, 0);
+			goTo(Box4, botHeight-10, 0);
+			goTo(Box4, botHeight+40, 0);
+		}
+			
+		
+		atHome = false;
+		//goTo(num,tableHeight-20);
+		//DynamixelSerial.halt(2000);
+		
+		//while(da.ds.readMoving()){}
+		//da.ds.motor(3, 0);
+		//goTo(num+50,tableHeight);
+		//while(da.ds.readMoving()){}
+		goHome();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
